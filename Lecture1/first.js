c8285748b0144afce959540bc6590d4d693bf36d
codewithria89 <anjalik0822@gmail.com>
2026-08-20 18:12:17 +0530

    Add JS : variables (let/const/var), data types, typeof operator, object property access

--- Lecture1/first.js
+++ Lecture1/first.js
@@ -1,3 +1,53 @@
 console.log("Apna college")
 console.log("Anjali")
-console.log("Anjali")
+
+
+//Variable
+//name = "Anjal";
+//console.log(name);
+
+let name = "Tony Stark";
+console.log(name)
+
+const plot = "Kalavati Bhawan"
+console.log(plot);
+
+const pi = 3.14;
+console.log(pi);
+
+{
+    let a = 5;
+    console.log(a);
+}
+
+{
+    let a = 10;
+    console.log(a)
+}
+
+const itemPrice = 250;
+let quantity = 2;
+//let total = itemPrice * quantity;
+//console.log(total)
+
+quantity = 3;
+let total = itemPrice * quantity;
+console.log(total);
+
+
+//DataType
+console.log(typeof 25);          
+console.log(typeof "hello");    
+console.log(typeof true);       
+console.log(typeof undefined);   
+console.log(typeof null);        
+console.log(typeof { });         
+///
+
+const student = {
+    fullName : "Rahul",
+    age : 32,
+    cgpa : 8.9,
+    isPass : true,
+};
+console.log(student["fullName"])
