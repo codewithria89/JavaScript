e721885794fd9af00ea2636ef78391380f0eae10
codewithria89 <anjalik0822@gmail.com>
2026-08-21 17:43:31 +0530

    Add arithmetic operators practice: +, -, *, /, %, ** and increment/decrement

---
+++ Lecture2/airthem.js
@@ -0,0 +1,14 @@
+let a = 10, b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+let count = 5;
+console.log(count++);
+console.log(count++);
+console.log(count--);
+console.log(count--)
