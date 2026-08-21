37c6fb79c350581127dffcc0942448b1387b566f
codewithria89 <anjalik0822@gmail.com>
2026-08-21 18:00:46 +0530

    Add comparison operators practice: == vs === and loose vs strict equality

---
+++ Lecture2/compare.js
@@ -0,0 +1,6 @@
+console.log(5 === 5);      // true
+console.log(5 !== "5");    // true (different types, so "not equal" is true)
+console.log(10 > 5);       // true
+console.log(10 <= 10);    // true
+console.log(5 == "5");    // true  ← loose equality it converts
+console.log(5 === "5");   // false ← strict equality it check data types
