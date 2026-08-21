1bc230dda4cd78f697f17abd30c41eb8f356699b
codewithria89 <anjalik0822@gmail.com>
2026-08-21 17:56:06 +0530

    Add assignment operators practice: +=, -=, *=, /=, %=

---
+++ Lecture2/assign.js
@@ -0,0 +1,17 @@
+let q = 5;
+let r = 2;
+
+/*q += 4;
+console.log("q = ", q);*/
+
+q *=4;
+console.log("q = ", q)
+
+//Assignment operators
+
+let s = 10;
+console.log(s += 5)
+console.log( s -= 3);
+console.log(s *= 5);
+console.log(s /= 4);
+console.log(s %= 4)
